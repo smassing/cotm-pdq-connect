8374f40993f41f262f03e90546b0a85da8f99591
Steve Massing <steve.massing@gmail.com>
2025-07-07 21:21:18 -0600

    Create ScriptRunner.ps1

---
+++ Login-Scripts/ScriptRunner.ps1
@@ -0,0 +1,15 @@
+$logonScripts = Get-ChildItem -Path "C:\COTM\Logon\*.ps1" -File
+
+$sortedScripts = $logonScripts | Sort-Object
+
+ForEach ($script in $sortedScripts) {
+    
+    Try {
+        $result = Start-Process -FilePath "powershell.exe" -ArgumentList "-File " + $script -Wait -NoNewWindow -ErrorAction Stop
+        Write-Host $result
+    } 
+    Catch {
+        Write-Host "Process run failure: " + $script
+    }
+
+}
